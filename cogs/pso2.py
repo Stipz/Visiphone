90989013afaaf3279c4963793743505b3e55527f
Stipz <stipz999@gmail.com>
2017-06-14 15:01:18 +0800

    Create pso2.py

--- cogs/pso2.py
+++ cogs/pso2.py
@@ -127,7 +127,7 @@ class PSO2:
                 iteminfo = []
 
                 if js:
-                    if len(js) >= 1 and len(js) <= 60:
+                    if len(js) >= 1 and len(js) <= 1:
                         for result in js:
                             if result["EnName"]:
                                 #iteminfo.append("``EN Name:`` {} | ``JP Name:`` {}\n\n``Ship 01:`` {:,.0f}\n``Ship 02:`` {:,.0f}\n``Ship 03:`` {:,.0f}\n``Ship 04:`` {:,.0f}\n``Ship 05:`` {:,.0f}\n``Ship 06:`` {:,.0f}\n``Ship 07:`` {:,.0f}\n``Ship 08:`` {:,.0f}\n``Ship 09:`` {:,.0f}\n``Ship 10:`` {:,.0f}\n".format(result["EnName"], result["JpName"]
@@ -141,7 +141,7 @@ class PSO2:
                                 #                                          , result["PriceInfo"][0]["Price"]
                                 #                                          , result["PriceInfo"][3]["Price"]
                                 #                                          , result["PriceInfo"][1]["Price"]))
-                                iteminfo.append("``EN Name:`` {} | ``JP Name:`` {}\n\n Price: {:,.0f}".format(result["EnName"], result["JpName"] , result["PriceInfo"][8]["Price"]))
+                                iteminfo.append("``EN Name:`` {} | ``JP Name:`` {}\nPrice: {:,.0f}\n".format(result["EnName"], result["JpName"] , result["PriceInfo"][8]["Price"]))
                         string = "\n".join(iteminfo)
                         #message = "{} Here are the results of your query:\n{}".format(ctx.message.author.mention, string)
                         message = "{} Here are the results of your [price] query: \n{}\n".format(ctx.message.author.mention, string)
