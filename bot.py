522f4bc66fddb06490ca58e33649ba14b7fa4434
Stipz <stipz999@gmail.com>
2017-06-25 14:34:16 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,10 +33,10 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    if member.server.id == '80900839538962432':
+    if member.server.id == '118779107218554880':
         role = discord.utils.get(member.server.roles, name='Members')
         await bot.add_roles(member, role)
-        await bot.send_message(discord.Object("80900839538962432"), '{}, welcome to the PSO2 Discord. Type `+pso2` if you need information regarding the game, and read the #rules.'.format(member.mention))
+        await bot.send_message(discord.Object("118779107218554880"), '{}, welcome to the PSO2 Discord. Type `+pso2` if you need information regarding the game, and read the #rules.'.format(member.mention))
 
 
 if __name__ == '__main__':
